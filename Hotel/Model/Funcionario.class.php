988076576d8dc2618a2c493e76cb31c865ff0c36
Tdisho <psAvell@Avell-PC>
2013-05-20 18:12:05 -0300

    Implementações da interface nas classes base

--- Hotel/Model/Funcionario.class.php
+++ Hotel/Model/Funcionario.class.php
@@ -1,10 +1,19 @@
 <?php
-class Funcionario {
+require_once '/Hospede.class.php';
+require_once '/PDOConnectionFactory.class.php';
+require_once '/model.interface.php';
+class Funcionario implements model {
+	private $connection;
 	private $id;
 	private $nome;
 	private $cpf;
 	private $sexo;
 
+	public function Funcionario(){
+		$con = new PDOConnectionFactory();
+		$this->connection = $con->getConnection();
+	}
+	
 	public function getId() {
 		return $this->id;
 	}
@@ -37,4 +46,49 @@ class Funcionario {
 		$this->id = $id;
 	}
 
+	public function save(){
+		$stmt = $this->connection->prepare("INSERT INTO funcionario (nome, cpf, sexo)
+			VALUES (?,?,?)") or die(mysql_error());
+	
+		$stmt->bindValue(1, $this->getNome());
+		$stmt->bindValue(2, $this->getCpf());
+		$stmt->bindValue(3, $this->getSexo());
+	
+		return $stmt->execute();
+	}
+	public function delete(){
+		$stmt = $this->connection->prepare("DELETE FROM funcionario WHERE id = ?") or die(mysql_error());
+		$stmt->bindValue(1, $this->getId());
+		return $stmt->execute();
+	}
+	
+	public function SelectById($Id){
+		$stmt = $this->connection->prepare("SELECT * FROM funcionario WHERE id = ?") or die(mysql_error());
+		$stmt->bindValue(1, $Id);
+		$stmt->execute();
+		$row = $stmt->fetch();
+		//var_dump($row);
+		$this->setId($row['id']);
+		$this->setNome($row['nome']);
+		$this->setCpf($row['cpf']);
+		$this->setSexo($row['sexo']);
+		return $row;
+	}
+	
+	public function ListAll(){
+		$all;
+		$ind = 0;
+		$stmt = $this->connection->prepare("SELECT * FROM funcionario", array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL)) or die(mysql_error());
+		$stmt->execute();
+		while ($row = $stmt->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT))
+		{
+				
+			$all[$ind]['id'] = $row[0];
+			$all[$ind]['nome'] = $row[1];
+			$all[$ind]['cpf'] = $row[2];
+			$all[$ind]['sexo'] = $row[3];
+			$ind++;
+		}
+		return $all;
+	}
 }
--- Hotel/Model/model.interface.php
+++ Hotel/Model/model.interface.php
@@ -4,4 +4,5 @@ interface model
 	public function save();
 	public function delete();
 	public function SelectById($Id);
+	public function ListAll();
 }
